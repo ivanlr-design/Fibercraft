82bb530d1ffb4dabfb56b69ab05e4c5f73f1a70c
ivanlr-design <141946852+ivanlr-design@users.noreply.github.com>
2024-04-13 13:21:06 +0200

    change(bot): change send request api data

--- Utils/Api/request_api.py
+++ Utils/Api/request_api.py
@@ -10,17 +10,11 @@ def request_api(url, api_key, command, args, map=None):
     
     hash_value = hashlib.sha1(hash_input).hexdigest()
 
-    body = {
-    'command': command,
-    'args': args,
-    'hash': hash_value
-    }
-
+    payload = f'command={command}&args={args}&hash={hash_value}'
     headers = {
-    'x-api-key': api_key,
-    'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
     
-    response = requests.post(url, data=body, headers=headers)
+    response = requests.post(url, data=payload, headers=headers)
 
     return response.text

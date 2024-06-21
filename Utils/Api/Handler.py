0b46c9cde9347e06450374c80483c68de0fc24c7
ivanlr-design <ivanlopezruiz187@gmail.com>
2024-06-21 13:03:16 +0200

    Update API handler

--- Utils/Api/Handler.py
+++ Utils/Api/Handler.py
@@ -1,6 +1,22 @@
 from flask import Flask, jsonify, request
+import pymysql.cursors
+import pymysql
+import os
+MYSQL_CONNECT = os.getenv("MYSQL_CONNECTOR")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWD = os.getenv("MYSQL_PASSWD")
 
 app = Flask(__name__)
+
+def db_connection():
+    connection = pymysql.connect(host=MYSQL_CONNECT,
+                                user=MYSQL_USER,
+                                password=MYSQL_PASSWD,
+                                db='s51219_punish',
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+
+    return connection
 
 @app.route("/api/v1/CheckKey", methods=["GET"])
 def CheckKey():
@@ -9,14 +25,26 @@ def CheckKey():
     Key = request.args.get("Key")
     HWID = request.args.get("HWID")
     TypeKey = request.args.get("TypeKey")
-    print(apikey)
-    print(Key)
-    print(HWID)
-    print(TypeKey)
+    
     if not apikey or not Key or not HWID or not TypeKey:
         return jsonify({"Failed to Authenticate": "Not enough arguments"}), 400
 
-    return jsonify({"Authenticated": "Succesfully auth"}), 200
+    conection = db_connection()
+    if conection:
+        cursor = conection.cursor()
+        consulta = "SELECT Type FROM KeysValidation WHERE ValidKey = %s"
+        VALUES = (Key, )
+        cursor.execute(consulta, VALUES)
+        results = cursor.fetchall()
+        if results:
+            print(results)
+        
+        return jsonify({"Authenticated": "Succesfully auth"}), 200
+    else:
+        return jsonify({"Internal Error": "Mysql Error"}), 500
+
+
+    
 
 def Run():
     app.run(host='0.0.0.0', port=21902)
